50b07af9e4d99dc50e58402973b069a2e829f1b0
chippendemi <64954094+chippendemi@users.noreply.github.com>
2020-08-12 10:02:48 +0800

    Add files via upload

---
+++ lamrop.py
@@ -0,0 +1,32 @@
+x = input('Input Lamrop Code: \n')
+
+def lamrop(inp):
+    arr = []
+    zpl = 0
+    s = 1
+    i = 0
+    arrout = ''
+    while i < len(inp):
+        if inp[i] == 'l':
+            if zpl > 0:
+                zpl -= 1
+        elif inp[i] == 'a':
+            arr[zpl] += s
+        elif inp[i] == 'm':
+            arr[zpl] -= s
+        elif inp[i] == 'r':
+            zpl += 1
+        elif inp[i] == 'o':
+            print(arrout)
+        elif inp[i] == 'p':
+            arr.append(0)
+        elif inp[i] == 's':
+            s = arr[zpl]
+        elif inp[i] == '+':
+            if inp[i+1] == 'c':
+                arrout = arrout + chr(arr[zpl])
+            else:
+                arrout = arrout + str(arr[zpl])
+        i += 1
+
+lamrop(x)
