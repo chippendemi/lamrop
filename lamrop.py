19b9ba1fa5771f54a337e5cf158580f8a7df9f33
chippendemi <64954094+chippendemi@users.noreply.github.com>
2020-08-13 13:21:40 +0800

    Update lamrop.py

--- lamrop.py
+++ lamrop.py
@@ -30,7 +30,39 @@ def lamrop(inp):
         elif inp[i] == '.':
             arrout = ''
         elif inp[i] == 'i':
-            s = float(input(arrout))
+            if inp[i+1] == 'f':
+                s = input(arrout)
+                ls = 0
+                try:
+                    s = float(s)
+                except ValueError:
+                    ls = 0
+                while ls == 0:
+                    try:
+                        s = float(s)
+                        ls = 1
+                    except ValueError:
+                        ls = 0
+                i += 1
+            elif inp[i+1] == 'i':
+                s = input(arrout)
+                ls = 0
+                try:
+                    s = int(s)
+                except ValueError:
+                    ls = 0
+                while ls == 0:
+                    try:
+                        s = int(s)
+                        ls = 1
+                    except ValueError:
+                        ls = 0
+                i += 1
+            elif inp[i+1] == 'c':
+                s = input(arrout)
+                if zpl < len(s):
+                    s = ord(s[zpl])
+                i += 1
         i += 1
 
 lamrop(x)
